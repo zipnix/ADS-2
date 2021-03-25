f638277158e7ba35f30d55ade8e23b21aa540cf4
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2021-03-25 07:15:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int *arr,int len,int value) {
+int countPairs1(int *arr, int len, int value) {
   return 0;
 }
 
-int countPairs2(int *arr,int len,int value) {
+int countPairs2(int *arr, int len, int value) {
   return 0;
 }
 
-int countPairs3(int *arr,int len,int value) {
+int countPairs3(int *arr, int len, int value) {
   return 0;
 }
