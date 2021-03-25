7b0d63f2d5d559ec15d59218d842b831b5ac41b2
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2021-03-25 07:12:24 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,7 +10,7 @@ int countPairs1(int *arr,int len,int value);
 int countPairs2(int *arr,int len,int value);
 int countPairs3(int *arr,int len,int value);
 
-void generateSorted(int *arr, int min, int max,int len)
+void generateSorted(int *arr, int min, int max,int len) 
 {
     int value = 0, j=0;
     for (int i = 0; i < len; i++)
@@ -27,45 +27,44 @@ void generateSorted(int *arr, int min, int max,int len)
 }
 
 
-TEST(lab2,test1)
+TEST(lab2, test1)
 {
-    int *arr=new int[ARRSIZE1];
-    generateSorted(arr,0,100,ARRSIZE1);
-    int count1=countPairs1(arr,ARRSIZE1,50);
-    int count2=countPairs2(arr,ARRSIZE1,50);
-    int count3=countPairs3(arr,ARRSIZE1,50);
-    EXPECT_TRUE((count1==count2) && (count1==count3) && (count2==count3));
-    delete[]arr;
+    int *arr = new int[ARRSIZE1];
+    generateSorted(arr, 0, 100, ARRSIZE1);
+    int count1 = countPairs1(arr, ARRSIZE1, 50);
+    int count2 = countPairs2(arr, ARRSIZE1, 50);
+    int count3 = countPairs3(arr, ARRSIZE1, 50);
+    EXPECT_TRUE((count1 == count2) && (count1 == count3) && (count2 == count3));
+    delete[] arr;
 }
-TEST(lab2,test2)
+TEST(lab2, test2)
 {
-    int *arr=new int[ARRSIZE2];
-    generateSorted(arr,0,100,ARRSIZE2);
-    int count1=countPairs1(arr,ARRSIZE2,50);
-    int count2=countPairs2(arr,ARRSIZE2,50);
-    int count3=countPairs3(arr,ARRSIZE2,50);
-    EXPECT_TRUE((count1==count2) && (count1==count3) && (count2==count3));
-    delete[]arr;
+    int *arr = new int[ARRSIZE2];
+    generateSorted(arr, 0, 100, ARRSIZE2);
+    int count1 = countPairs1(arr, ARRSIZE2, 50);
+    int count2 = countPairs2(arr, ARRSIZE2, 50);
+    int count3 = countPairs3(arr, ARRSIZE2,50);
+    EXPECT_TRUE((count1 == count2) && (count1 == count3) && (count2 == count3));
+    delete[] arr;
 }
-TEST(lab2,test3)
+TEST(lab2, test3)
 {
-    clock_t begin1,end1,begin2,end2,begin3,end3;
-    clock_t t1,t2,t3;
-    int *arr=new int[ARRSIZE1];
-    generateSorted(arr,0,100,ARRSIZE1);
+    clock_t begin1, end1, begin2, end2, begin3, end3;
+    clock_t t1, t2, t3;
+    int *arr = new int[ARRSIZE1];
+    generateSorted(arr, 0, 100, ARRSIZE1);
     begin1 = clock();
-    int count1=countPairs1(arr,ARRSIZE1,50);
-    end1=clock();
+    int count1 = countPairs1(arr, ARRSIZE1, 50);
+    end1 = clock();
     begin2 = clock();
-    int count2=countPairs2(arr,ARRSIZE1,50);
-    end2=clock();
+    int count2 = countPairs2(arr, ARRSIZE1, 50);
+    end2 = clock();
     begin3 = clock();
-    int count3=countPairs3(arr,ARRSIZE1,50);
-    end3=clock();
-    t1=end1-begin1;
-    t2=end2-begin2;
-    t3=end3-begin3;
-    EXPECT_TRUE((t1>t2) && (t1>t3) && (t2>t3));
-    delete[]arr;
+    int count3 = countPairs3(arr, ARRSIZE1, 50);
+    end3 = clock();
+    t1 = end1 - begin1;
+    t2 = end2 - begin2;
+    t3 = end3 - begin3;
+    EXPECT_TRUE((t1 > t2) && (t1 > t3) && (t2 > t3));
+    delete[] arr;
 }
-
