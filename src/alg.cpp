200249fcc1add2c939b553e3fe1518c3bd0fd4fc
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2021-03-25 07:06:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int countPairs1(int *arr,int len,int value) {
+  return 0;
+}
+
+int countPairs2(int *arr,int len,int value) {
+  return 0;
+}
+
+int countPairs3(int *arr,int len,int value) {
+  return 0;
 }
