55774f57e8042f8bf54e48fe351bfcc0f1c70781
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2021-03-25 07:14:41 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -2,8 +2,8 @@
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
-int countPairs1(int *arr,int len,int value);
-int countPairs2(int *arr,int len,int value);
-int countPairs3(int *arr,int len,int value);
+int countPairs1(int *arr, int len, int value);
+int countPairs2(int *arr, int len, int value);
+int countPairs3(int *arr, int len, int value);
 
 #endif  // INCLUDE_ALG_H_
