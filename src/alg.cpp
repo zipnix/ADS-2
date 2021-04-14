c6abf7590d10f0d0c0dc85c9776d53b59ebeb209
zipnix <78799489+zipnix@users.noreply.github.com>
2021-04-14 21:02:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,57 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int score = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j]) == value))
+        score++;
+    }
+  }
+  if (score)
+    return score;
+  else
+    return 0;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  for (int i = len - 1; i >= 0; i--) {
+    if (arr[i] <= value) {
+      for (int j = 0; j < i; j++) {
+        if (arr[i] + arr[j] == value)
+          score++;
+      }
+    }
+  }
+  if (score)
+    return score;
+  else
+    return 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int score = 0, left = 0, right, center;
+  for (int i = len - 1; i >= 0; i--) {
+    if (arr[i] <= value) {
+      len = i;
+      brake;
+      }
+      for ( int i = 0; i < len - 1; i++) {
+        left = i + 1;
+        right = len - 1;
+      while (left < right) {
+        center = (left + right) / 2;
+        if (arr[center] < value - arr[i])
+          left = center + 1;
+        else right = center;
+      }
+      }
+      while (arr[left] == value - arr[i]) {
+        score++;
+        left++;
+      }
+  if (score)
+    return score;
+  else
+    return 0;
 }
