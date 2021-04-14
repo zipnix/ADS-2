2c84185e7c0c340160787b4d77e3aded4b1913da
zipnix <78799489+zipnix@users.noreply.github.com>
2021-04-14 21:15:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int *arr, int len, int value) {
   int score = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
-      if ((arr[i] + arr[j]) == value))
+      if ((arr[i] + arr[j]) == value)
         score++;
     }
   }
@@ -12,6 +12,7 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
+   int score = 0;
   for (int i = len - 1; i >= 0; i--) {
     if (arr[i] <= value) {
       for (int j = 0; j < i; j++) {
